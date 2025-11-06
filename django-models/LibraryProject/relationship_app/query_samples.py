f776f1ca83a49e59e155bcf1d7c3a7b9c2efbabe
altroste001 <ayoubrofix055@gmail.com>
2025-11-06 16:10:16 +0100

    Fix: Update queries to match ALX expectations

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,9 @@
 from relationship_app.models import Author, Book, Librarian, Library
 
 author = Author.objects.get(name=author_name)
-books_by_author  = author.book_set.all()
+books_by_author = Book.objects.filter(author=author)   
 
-    
 library = Library.objects.get(name=library_name)
-books_in_library  = library.books.all()
-    
+books_in_library = library.books.all()               
 
-
-librarian = library.librarian
-
-
-
-
+librarian = Librarian.objects.get(library=library)    
