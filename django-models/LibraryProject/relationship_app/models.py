02a35ffac470e7e27001151490c51e82aed489ab
altroste001 <ayoubrofix055@gmail.com>
2025-11-06 16:01:59 +0100

    Fix: Add correct models and query_samples for ALX

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -10,13 +10,13 @@ class Book(models.Model):
     def __str__(self):
         return self.title
     
-class Library (models.Model):
+class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
     def __str__(self):
         return self.name
     
-class Librarian (models.Model):
+class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)         
     def __str__(self):
