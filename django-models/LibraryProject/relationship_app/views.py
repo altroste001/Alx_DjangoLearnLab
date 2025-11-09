48adb85078a2d7938e46b84f578589670ee55aee
altroste001 <ayoubrofix055@gmail.com>
2025-11-09 18:28:19 +0100

    Task 3: Implement role-based access control with UserProfile and role-specific views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from .models import Book, Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import user_passes_test
 
 def books_list(request):
     books = Book.objects.all()
@@ -24,3 +25,22 @@ def register(request):
         form = UserCreationForm()
     
     return render(request, "relationship_app/register.html", {"form": form})
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
