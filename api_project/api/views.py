13a6ecaad5a51f471890644aa54a2cd7d87e0aa5
altroste001 <ayoubrofix055@gmail.com>
2025-11-23 16:48:29 +0100

    Task 3: Implement authentication and permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework import permissions
 from .serializers import BookSerializer
 from .models import Book
 
@@ -11,3 +12,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
