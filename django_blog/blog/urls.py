d32ac0220fe407bdbe001abf4797f39964c361ff
7roste <152579950+altroste001@users.noreply.github.com>
2025-12-05 17:25:26 +0100

    Complete Task 2 - Blog post CRUD implementation

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,6 +5,12 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
-    
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+
+    # CRUD
+    path('posts/', views.PostListView.as_view(), name='posts'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
