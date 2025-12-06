7cd5e9f1e4e28bcac38dc04c2687c1a4d92e1923
7roste <152579950+altroste001@users.noreply.github.com>
2025-12-06 18:04:17 +0100

    Fix comment URLs to match ALX structure

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     # List view (plural)
     path('posts/', views.PostListView.as_view(), name='posts'),
     
-    # Comments
-    path('post/<int:pk>/comment/', views.CommentCreateView.as_view(), name='comment-create'),
-    path('comment/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
-    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+    # Comments 
+    path('posts/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -96,10 +96,14 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     template_name = 'blog/comment_form.html'
 
     def form_valid(self, form):
-        post = Post.objects.get(pk=self.kwargs['pk'])
+        post = Post.objects.get(pk=self.kwargs['post_id'])
         form.instance.post = post
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
+
 
 
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
