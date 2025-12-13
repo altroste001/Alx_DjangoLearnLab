62fecc1b73f12d5318c4cfd74d50ae4c84d68564
7roste <152579950+altroste001@users.noreply.github.com>
2025-12-13 01:16:46 +0100

    Task 2 completed: Follow and unfollow views implemented

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,37 +1,37 @@
-from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 
 from .serializers import RegisterSerializer, UserSerializer
 
-User = get_user_model()
+# ALX expects CustomUser.objects.all()
+CustomUser = get_user_model()
 
 
 class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
+    queryset = CustomUser.objects.all()
     serializer_class = RegisterSerializer
 
 
-class UserProfileView(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
-
-
 class FollowUserView(generics.GenericAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_follow = User.objects.get(id=user_id)
+        user_to_follow = CustomUser.objects.get(id=user_id)
         request.user.following.add(user_to_follow)
-        return Response({"detail": "User followed"}, status=status.HTTP_200_OK)
+        return Response(
+            {"detail": "User followed"},
+            status=status.HTTP_200_OK
+        )
 
 
 class UnfollowUserView(generics.GenericAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_unfollow = User.objects.get(id=user_id)
+        user_to_unfollow = CustomUser.objects.get(id=user_id)
         request.user.following.remove(user_to_unfollow)
-        return Response({"detail": "User unfollowed"}, status=status.HTTP_200_OK)
+        return Response(
+            {"detail": "User unfollowed"},
+            status=status.HTTP_200_OK
+        )
